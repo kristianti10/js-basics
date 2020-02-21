1c90d6119401c8aa630d2e6d1166c7a3c6bb6012
Kristianti Yulistia <yulistiakristianti1@gmail.com>
2020-02-21 07:29:09 -0800

    leap years sudah

--- leap_years.js
+++ leap_years.js
@@ -1,9 +1,15 @@
 // Buat function untuk me-return tahun kabisat dari range tahun yang diberikan
 // misalanya 2015-2020 ada 2 tahun kabisat yaitu 2016 dan 2019
 
-function showKabisat() {
+function leapyear(year) {
+  let leapyear   
+    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+}
+console.log(leapyear(2016));
+console.log(leapyear(2000));
+console.log(leapyear(1700));
+console.log(leapyear(1800));
+console.log(leapyear(100));
+
   // kalian bisa lihat defenisi tahun kabisat di wikipedia.
   // serta algoritma untuk menentukan tahun kabisatya.
-}
-
-
